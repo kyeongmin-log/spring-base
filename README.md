4edd901fc61bcb4fca6a56f200caee0486dad8e8
kyeongmin-log <dotcom7860@gmail.com>
2020-12-29 14:25:32 +0900

    순수 JDBC

--- README.md
+++ README.md
@@ -224,6 +224,15 @@ public class SpringConfig {
 - 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 ComponentScan 을 사용한다.
 그리고 정형화 되지 않거나, 상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈으로 등록한다.
 
+# 스프링 설정 이미지
+
+![springconfingimg](./ReadmeImg/spring-config.PNG)
+
+- 개방-폐쇄 원칙(OCP, Open-Closed Principle)
+  - 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+  
+- 스프링의 DI(Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
+
 # IntelliJ 단축키
 - ctrl + shift + enter : 자동완성기능
 
--- src/main/java/base/springbase/SpringConfig.java
+++ src/main/java/base/springbase/SpringConfig.java
@@ -1,13 +1,24 @@
 package base.springbase;
 
+import base.springbase.repository.JdbcMemberRepository;
 import base.springbase.repository.MemberRepository;
 import base.springbase.repository.MemoryMemberRepository;
 import base.springbase.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
